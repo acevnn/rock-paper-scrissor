ae841034a4bdaeaea243302a819ac1ca55d01a30
Nino <ninoacev@gmail.com>
2024-03-14 15:46:17 +0100

    a round of rock, paper, scissor

---
+++ index.js
@@ -0,0 +1,34 @@
+const gameArray = ['Rock', 'Paper', 'Scissor'];
+
+const getComputerChoice = () => {
+	let randomValue = Math.floor(Math.random() * gameArray.length);
+	return gameArray[randomValue];
+}
+
+let playerChoice = prompt("Choose Rock, Paper, or Scissor").toLowerCase();
+console.log('Players choice:', playerChoice);
+
+let computerChoice = getComputerChoice().toLowerCase();
+console.log('The AI chose:', computerChoice);
+
+const play = (playerSelection, computerSelection) => {
+
+	const winningCombinations = {
+		paper: 'paper',
+		rock: 'rock',
+		scissor: 'scissor',
+	};
+	let result = '';
+
+	if (playerSelection === computerSelection) {
+		result = "Both the human and AI chose " + playerSelection + ", so it is a draw";
+	} else if (winningCombinations[playerSelection] === computerSelection) {
+		result = 'Human won! \n - ' + playerSelection + ' beats ' + computerSelection;
+	} else {
+		result = 'AI won! \n - ' + computerSelection + ' beats ' + playerSelection;
+	}
+
+	return result;
+	}
+
+console.log(play(playerChoice, computerChoice));
